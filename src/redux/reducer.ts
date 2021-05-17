424d8dcbf939d3d23a8d92e3d240ef8962c4eea4
Julia gru <julia.gru@gmail.com>
2021-05-17 14:52:13 +0200

    add redux, create store files

---
+++ src/redux/reducer.ts
@@ -0,0 +1,10 @@
+import { AnyAction } from 'redux';
+
+const INITIAL_STATE = {};
+
+export const reducer = (state = INITIAL_STATE, action: AnyAction) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
